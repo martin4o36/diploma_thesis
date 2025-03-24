53274335a220e804867429b817e5b5814ea7da23
Kozarev Martin <martin.kozarev@extern.a1.at>
2025-03-24 14:55:04 +0200

    Added app password to gmail account and in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,9 +108,10 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'team.trackerbg@gmail.com'
-EMAIL_HOST_PASSWORD = '2006!@ab'
+EMAIL_HOST_PASSWORD = 'ywbk cjsy untw zwlj'
 
 
 # Password validation
